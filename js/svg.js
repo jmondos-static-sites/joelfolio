f66f4c6526efa1780fef48796b6735cfd585456f
gkiely <grant.kiely@gmail.com>
2014-06-21 15:59:54 -0700

    currently working on case studies

--- js/svg.js
+++ js/svg.js
@@ -1,16 +1,22 @@
 /*
 @svgjs
-@version: 1
+@version: 1.1
 @author: Grant Kiely
-@date: 1/6/14
+@date: 21/6/14
 
 Awesomeness
--Easy peasy way to cache svg's
+-Easy way to cache svg's
 -Allows styling of svg
 -Loads async
 
 Todo
--Make it work in every browser and fallback to png for ie using image.vida.io
+Perf-wise
+-make just one queryselector call, slice to array and filter for each arr elem
+    http://jsperf.com/find-children-vs-queryselectoral/3
+-change innerHTML to doc fragment
+    -First create a function that converts string to document fragment and test against inner
+    http://jsperf.com/document-fragment-vs-innerhtml-vs-looped-appendchild
+-Make it work in every browser and fallback to png for ie using image.vida.io?
 http://stackoverflow.com/questions/3975499/convert-svg-to-image-jpeg-png-etc-in-the-browser
 -Checkout <svg> <img /> </svg for fallback
 
@@ -26,32 +32,33 @@ http://stackoverflow.com/questions/3975499/convert-svg-to-image-jpeg-png-etc-in-
 
 (function(){
 
-
 	var arr = [
         '<svg class="location-pin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="24px" height="31px" viewBox="0 0 24 31" enable-background="new 0 0 24 31" xml:space="preserve"><path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M12,0C5.373,0,0,5.206,0,11.625C0,18.045,12,31,12,31  s12-12.955,12-19.375C24,5.206,18.627,0,12,0z M20,11.625c0,4.28-3.582,7.75-8,7.75s-8-3.47-8-7.75s3.582-7.75,8-7.75  S20,7.346,20,11.625z"/></svg>'
 	];
+    var div = document.createElement('div');
 
+    function init(){
+        var list, ll, sel,
+            l = arr.length;
+
+        for(var i =0; i<l; i++){
+            div.innerHTML = arr[i];
+            sel = (div.firstChild.classList ?  div.firstChild.classList[0] : false) || div.firstChild.className.animVal;
+            list = document.querySelectorAll('[data-svg="'+sel+'"]');
+            ll = list.length;
+            for(var j=0; j<ll; j++){
+                list[j].appendChild(div.firstChild.cloneNode(true));
+            }
+        }
+    }
+
+    //Initialise
 	var ready = document.readyState;
 	if(ready === "complete" || ready ==="interactive"){
 		init();
 	}
-	else document.addEventListener("DOMContentLoaded", init);
-
-  	function init(){
-  		var l = arr.length,
-    		list, ll,
-    		div = document.createElement('div'),
-    		sel;
-
-    	for(var i =0; i<l; i++){
-    		div.innerHTML = arr[i];
-            sel = (div.firstChild.classList ?  div.firstChild.classList[0] : false) || div.firstChild.className.animVal || div.firstChild.id;
-            list = document.querySelectorAll('[data-svg="'+sel+'"]');
-            ll = list.length;
-            debugger
-    		for(var j=0; j<ll; j++){
-    			list[j].appendChild(div.firstChild.cloneNode(true));
-    		}
-    	}
-  	}
+	else{
+        document.addEventListener("DOMContentLoaded", init);
+    }
+  	
 })();
